114cc2f5b39cb6c79b1e1918352760e3e8a59a8b
subhajit bhukta <subhajit.afw@gmail.com>
2026-01-30 14:55:23 +0530

    ready for test hosting

--- server.js
+++ server.js
@@ -34,6 +34,9 @@ wss.on("connection", (ws) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
+
